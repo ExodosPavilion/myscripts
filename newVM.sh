5f2178cd32ecf34c73bdc71307ba5d6214c2add3
ExodosPavilion <48080229+ExodosPavilion@users.noreply.github.com>
2024-07-11 12:36:12 -0500

    Update newVM.sh
    Remade the template so the following is no longer required:
    - Remove Tailscale
    - Remove and Regen SSH keys
    - Re-Install archlinux-keyring

--- newVM.sh
+++ newVM.sh
@@ -6,18 +6,6 @@ read -p "Enter the new hostname: " new_hostname
 # Update the hostname file
 sudo bash -c "echo $new_hostname > /etc/hostname"
 
-# Uninstall tailscale
-sudo pacman -R tailscale
-
-# Delete SSH host keys
-sudo rm -f -v /etc/ssh/ssh_host_*_key*
-
-# Generate new SSH host keys
-sudo ssh-keygen -A -v
-
-# Install archlinux-keyring
-sudo pacman -S archlinux-keyring --noconfirm
-
 # Update all packages
 sudo pacman -Syu --noconfirm
 
